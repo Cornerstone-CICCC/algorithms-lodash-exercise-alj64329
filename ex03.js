d28962a2db82ab77c98bd2b73854baf1baca916b
Ayaka Miyazaki <kuromi.1031blc@gmail.com>
2025-06-04 20:00:25 -0700

    Updated all solutions

--- ex03.js
+++ ex03.js
@@ -5,5 +5,9 @@ const _ = require('lodash');
 const numbers = [1, 3, 5, 7, 9];
 
 // your code here
+const firstGreaterThanFive = _.chain(numbers)
+.filter(num=>num>5)
+.first()
+.value()
 
 console.log(firstGreaterThanFive); // Expected output: 7
